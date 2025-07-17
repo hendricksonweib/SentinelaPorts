f9cc662d010990382426e3c83b1160a615dd3fba
hendricksonweib <hendricksonrocha22@gmail.com>
2025-07-17 17:16:09 -0300

    first commit

---
+++ components/login-form.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"form">) {
+  return (
+    <form className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="flex flex-col items-center gap-2 text-center">
+      <img src="./logo.png" alt="" />
+      </div>
+      <div className="grid gap-6">
+        <div className="grid gap-3">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="Digite seu usuário" required />
+        </div>
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="password">Senha</Label>
+          </div>
+          <Input id="password" type="password" placeholder="Digite sua senha"  required />
+        </div>
+        <Button type="submit" className="w-full">
+          Entrar
+        </Button>
+      </div>
+    </form>
+  )
+}
